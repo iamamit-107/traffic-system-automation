70753b78837852c024d2e340bc10bbf75a79edfd
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2023-02-20 09:25:35 +0600

    add traffic system

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TrafficLight from "react-trafficlight";
+import { useState } from "react";
+
+const intiState = [
+	{ red: false, yellow: false, green: false },
+	{ red: false, yellow: false, green: false },
+	{ red: false, yellow: false, green: false },
+	{ red: false, yellow: false, green: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [lights, setLights] = useState(intiState);
+
+	function wait(milliseconds) {
+		return new Promise((resolve) => setTimeout(resolve, milliseconds));
+	}
+
+	const clearAllTimeout = () => {
+		let id = window.setTimeout(() => {}, 0);
+
+		while (id--) {
+			window.clearTimeout(id);
+		}
+	};
+
+	const startSignal = async () => {
+		for (let i = 0; i < lights.length; i++) {
+			const traverseForGreen = lights.map((light, idx) => {
+				if (idx === i) {
+					return { red: false, green: true, yellow: false };
+				} else {
+					return { red: true, green: false, yellow: false };
+				}
+			});
+
+			setLights([...traverseForGreen]);
+			await wait(5000);
+
+			const traverseForYellow = lights.map((light, idx) => {
+				if (idx === i) {
+					return { red: false, green: false, yellow: true };
+				} else {
+					return { red: true, green: false, yellow: false };
+				}
+			});
+
+			setLights([...traverseForYellow]);
+			await wait(1000);
+
+			if (i === lights.length - 1) {
+				startSignal();
+			}
+		}
+	};
+
+	const stopSignal = () => {
+		setLights(intiState);
+		clearAllTimeout();
+	};
+
+	return (
+		<div className="App">
+			<div class="lamps">
+				{lights.map((light, idx) => (
+					<div
+						key={idx}
+						className={
+							idx === 0
+								? "first-lamp"
+								: idx === 2
+								? "third-lamp"
+								: idx === 3
+								? "fourth-lamp"
+								: "second-lamp"
+						}
+					>
+						<TrafficLight
+							RedOn={light.red}
+							GreenOn={light.green}
+							YellowOn={light.yellow}
+						/>
+					</div>
+				))}
+			</div>
+			<div className="btn-group">
+				<button className="start-btn" onClick={() => startSignal()}>
+					Start
+				</button>
+				<button className="stop-btn" onClick={() => stopSignal()}>
+					Stop
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
